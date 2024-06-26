156e2a6b0193407eef2f1de4ca80413870f51f87
Nikola Arsov <arsovnikola1@gmail.com>
2024-06-26 17:37:25 +0200

    Created project

---
+++ calSleek/src/main/java/com/example/calsleek/CalSleekApplication.java
@@ -0,0 +1,13 @@
+package com.example.calsleek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalSleekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalSleekApplication.class, args);
+    }
+
+}
