217d4d80d31195907980a96e35cee5d1aa88e714
nikol4e2 <arsovnikola1@gmail.com>
2025-07-17 23:58:09 +0200

    repos"

---
+++ calSleek/src/main/java/com/webapp/calsleek/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.webapp.calsleek.repositories;
+
+import com.webapp.calsleek.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameAndPassword(String username, String password);
+    Optional<User> findByEmailAndPassword(String email, String password);
+}
