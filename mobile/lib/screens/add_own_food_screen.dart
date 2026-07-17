06be4f4ba7a829aa736dc33697402e7cd9445169
nikol4e2 <arsovnikola1@gmail.com>
2026-07-17 21:06:53 +0200

    made add own food screen

---
+++ mobile/lib/screens/add_own_food_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddOwnFoodScreen extends StatefulWidget {
+  const AddOwnFoodScreen({super.key});
+
+  @override
+  State<AddOwnFoodScreen> createState() => _AddOwnFoodScreenState();
+}
+
+class _AddOwnFoodScreenState extends State<AddOwnFoodScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ mobile/lib/screens/my_foods_screen.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/screens/add_own_food_screen.dart';
+import '../AppColors.dart';
+import '../services/food_service.dart';
+import '../utils/storage.dart';
+
+
+
+class MyFoodsScreen extends StatefulWidget {
+  const MyFoodsScreen({super.key});
+
+  @override
+  State<MyFoodsScreen> createState() => _MyFoodsScreenState();
+}
+
+class _MyFoodsScreenState extends State<MyFoodsScreen> {
+
+
+  final FoodService service=FoodService();
+
+  List foods = [];
+
+  bool loading=true;
+
+
+  @override
+  void initState() {
+
+    super.initState();
+    loadFoods();
+  }
+
+
+  void loadFoods() async{
+
+    final userId= await Storage.getUserId();
+    final res = await service.getUserFoods(userId!);
+    
+    setState(() {
+      foods=res;
+      loading=false;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      backgroundColor: AppColors.background,
+      
+      appBar: AppBar(
+        title: const Text("My Foods"),
+        
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            
+            onPressed: () async{
+              await Navigator.push(context, 
+              MaterialPageRoute(builder: (_)=> const AddOwnFoodScreen()));
+              
+              loadFoods();
+            },
+            
+            
+          )
+        ],
+      ),
+      body: loading ? const Center(child: CircularProgressIndicator(),)
+      :foods.isEmpty ? const Center(
+        child: Text("You haven't created any foods yet",style: TextStyle(color: Colors.white54),),
+    ): ListView.builder(
+      padding: const EdgeInsets.all(16),
+    
+      itemCount: foods.length,
+      itemBuilder: (_,index)
+      {
+      final food= foods[index];
+      
+      return Container(
+        margin: const EdgeInsets.only(bottom: 12),
+        padding: const EdgeInsets.all(16),
+        decoration:BoxDecoration(color: Colors.white10,borderRadius: BorderRadius.circular(16)) ,
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        
+        Text(food['name'], style: const TextStyle( color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+      
+        const SizedBox(height: 8,),
+
+         Text(
+
+            "${food['calories']} kcal",
+
+            style: const TextStyle(
+
+            color: Colors.white70,
+
+            ),
+
+        ),
+        const SizedBox(height: 8,),
+      Container(
+
+      padding: const EdgeInsets.symmetric(
+      horizontal:10,
+      vertical:5,
+      ),
+
+      decoration: BoxDecoration(
+
+      color: food['isVerified']
+
+      ? Colors.green.withOpacity(0.2)
+
+          : Colors.orange.withOpacity(0.2),
+
+      borderRadius: BorderRadius.circular(20),
+
+      ),
+      child: Text(
+
+      food['isVerified']
+      ? "Approved"
+          : "Pending approval",
+
+
+      style: TextStyle(
+
+      color: food['isVerified']
+      ? Colors.greenAccent
+          : Colors.orangeAccent,
+
+      ),
+
+      ),
+
+      ),
+      ],
+      ),
+      );
+      },
+    
+    )
+    );
+  }
+}
